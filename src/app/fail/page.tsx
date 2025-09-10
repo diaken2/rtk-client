f2360e35d937ea9623b2a7bcbd0e04dd48b392ea
diaken2 <kenansalahov111@gmail.com>
2025-09-10 09:09:34 +0500

    upgrade cityselectmodal and heroadresssearch fix errors

--- src/app/fail/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Fail() {
-  return (
-    <div className="min-h-screen p-6">
-      <h1 className="text-2xl font-bold mb-2">Оплата не прошла ❌</h1>
-      <p>Попробуйте ещё раз или свяжитесь с поддержкой.</p>
-    </div>
-  );
-}
